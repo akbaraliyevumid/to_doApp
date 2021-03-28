ee9c7130a2af71f1fc884510b757ae5dcd35ac38
Umidjon <69582810+akbaraliyevumid@users.noreply.github.com>
2021-03-28 20:48:02 +0500

    To do
    To do application on WPF

---
+++ To_doApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using To_doApp.Models;
+using To_doApp.Services;
+
+namespace To_doApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\_todoDataList.json";
+        private BindingList<TodoModel> _todoData;
+        private FileIOServices _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOServices(PATH);
+
+            try
+            {
+                _todoData = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+
+            dgTodoList.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoData_ListChanged;
+        }
+
+        private void _todoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType==ListChangedType.ItemAdded || e.ListChangedType==ListChangedType.ItemDeleted||e.ListChangedType==ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+
+            }
+            
+        }
+    }
+}
